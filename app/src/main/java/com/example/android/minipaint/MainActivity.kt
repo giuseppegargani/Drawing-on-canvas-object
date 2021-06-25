565eb7d97b375a17ae936c2c79e3005b1c500ed5
Houde Giuseppe Gargani <sentierodisole1@yahoo.it>
2021-06-25 22:12:51 +0200

    Commit Iniziale

--- app/src/main/java/com/example/android/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -3,6 +3,18 @@ package com.example.android.minipaint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//TODO RIASSUNTO DEI TASKS
+
+/*TODO - 1 Si aggiungono due colori nel file delle risorse dei colori
+    <color name="colorBackground">#FFFF5500</color>
+    <color name="colorPaint">#FFFFEB3B</color>
+ */
+
+/* TODO - 2 Rimuovi l'ActionBar
+    Nel genitore del stile AppTheme rimpiazza DarkActionBar con NoActionBar
+
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
